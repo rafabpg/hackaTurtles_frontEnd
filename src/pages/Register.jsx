6632753d537dbe599d04e08a7c8524258ebad2bf
Rafael Pillar <rafael.baptista@injunior.com.br>
2023-10-07 20:02:39 -0300

    feat: adding name field funcional

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,14 +5,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { Link } from 'react-router-dom';
 const defaultTheme = createTheme();
 
 const Register = () => {
@@ -91,9 +91,9 @@ const Register = () => {
           </Button>
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
+            <Link  style={{ textDecoration: 'underline', color: 'blue' }} to={'/login'}>
+                      {"Already have an account? Sign in"}
+                    </Link>
             </Grid>
           </Grid>
         </Box>
