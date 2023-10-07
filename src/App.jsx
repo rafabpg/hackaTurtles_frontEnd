fcbbba592c32a2e24a4143e54350a53cf66e57b3
Rafael Pillar <rafael.baptista@injunior.com.br>
2023-10-07 19:46:11 -0300

    feat:adding register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Router from "./router"
 import Sidebar from "./components/Sidebar"
+import { useLocation } from 'react-router-dom';
 function App() {
+
+  const location = useLocation();
+  const routesToHideSidebar = ['/register', '/login'];
+  const shouldHideSidebar = routesToHideSidebar.includes(location.pathname);
 
   return (
     <>
-        <Sidebar/>
+      {!shouldHideSidebar && <Sidebar />}
         <Router/>
     </>
   )
--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 const Router = () => {
   return (
     <Routes >
       <Route path="/" element={ <Home /> } />
+      <Route path="/register" element={ <Register /> } />
   </Routes>
   )
 }
